2810e1ab58e3db1a8a1536410f5d80bfb683ca37
Isaac Snow <isaacjsnow@gmail.com>
2016-01-22 09:10:12 -0600

    Update to ES6 classes

--- src/modules/PacketConnection/index.js
+++ src/modules/PacketConnection/index.js
@@ -1,82 +1,83 @@
 var url = require('url'),
-    _ = require('lodash'),
     coll = 'packets';
+
+class PacketConnection {
+  /**
+   * Handles the connection with the packets collection.
+   * @constructor
+   * @param {string} path - Path from the req.url. Parsed to create the packet.
+   */
+  constructor(path, db) {
+    this._packet = this._parseUrl(path);
+    this.pendingTransaction = 0;
+  }
+  
+  /**
+   * Initiates the transactions to the db.
+   * @function
+   * @void
+   * @param {https://github.com/mongodb/node-mongodb-native/blob/2.1/lib/db.js} db - The mongo db object.
+   * @param {function} callback - Callback used to disconnect the connection to the db if all trancasctions have been completed.
+   */
+  initTransactions(db, callback) {
+    this._collection = db.collection(coll);
+    this._callback = callback;
     
-/**
- * Handles the connection with the packets collection.
- * @constructor
- * @param {string} path - Path from the req.url. Parsed to create the packet.
- */
-function PacketConnection (path, db) {
-  this._packet = this._parseUrl(path);
-  this.pendingTransaction = 0;
+    this._save();
+  }
+  
+  /**
+   * Gets the ID of the unit this packet came from.
+   * @function
+   * @return {Number} The ID of the unit.
+   */
+  getUnitId() {
+    return this._packet.unitId;
+  }
+  
+  /* 
+   * "Private" methods 
+   */
+
+  /**
+   * Inserts the packet into the db.
+   * @function
+   * @void
+   */
+  _save() {
+    // Register the pending transaction.
+    this.pendingQueries = this.pendingTransaction + 1;
+    
+    this._collection.insertOne({
+      unitId: this._packet.unitId,
+      temperature: this._packet.temperature,
+      water: this._packet.water,
+      light: this._packet.light,
+      raw: this._packet.raw,
+      createdAt: new Date()
+    }, 
+    // Use an arrow function to bind 'this' to the function
+    (err) => {
+      if (err) throw err;
+      
+      // Remove the pending transaction from the count.
+      this.pendingTransaction = this.pendingTransaction - 1;
+      
+      // Initiate callback (_disconnect in server.js)
+      this._callback(this);
+    });
+  }
+
+  /**
+   * Parses the url and returns the packet as an object.
+   * @function
+   * @param {string} path - The path from the req.url. This is parsed to create the packet object.
+   * @return {object} The settings for this unit.
+   */
+  _parseUrl(path) {
+    // Parse url to create the packet
+    return {};
+  }
 }
-
-/**
- * Initiates the transactions to the db.
- * @function
- * @void
- * @param {https://github.com/mongodb/node-mongodb-native/blob/2.1/lib/db.js} db - The mongo db object.
- * @param {function} callback - Callback used to disconnect the connection to the db if all trancasctions have been completed.
- */
-PacketConnection.prototype.initTransactions = function (db, callback) {
-  this._collection = db.collection(coll);
-  this._callback = callback;
-  
-  this._save();
-};
-
-/**
- * Gets the ID of the unit this packet came from.
- * @function
- * @return {Number} The ID of the unit.
- */
-PacketConnection.prototype.getUnitId = function () {
-  return this._packet.unitId;
-};
-
-/* 
- * "Private" methods 
- */
-
-/**
- * Inserts the packet into the db.
- * @function
- * @void
- */
-PacketConnection.prototype._save = function () {
-  // Register the pending transaction.
-  this.pendingQueries = this.pendingTransaction + 1;
-  
-  return this._collection.insert({
-    unitId: this._packet.unitId,
-    temperature: this._packet.temperature,
-    water: this._packet.water,
-    light: this._packet.light,
-    raw: this._packet.raw,
-    createdAt: new Date()
-  }, 
-  // Use _.bind to bind 'this' to the function until we get ES6 in here and use an arrow function. 
-  _.bind(function (err) {
-    if (err) throw err;
-    
-    // Remove the pending transaction from the count.
-    this.pendingTransaction = this.pendingTransaction - 1;
-    
-    // Initiate callback (_disconnect in server.js)
-    this._callback(this);
-  }, this));
-};
-
-/**
- * Parses the url and returns the packet as an object.
- * @function
- * @param {string} path - The path from the req.url. This is parsed to create the packet object.
- * @return {object} The settings for this unit.
- */
-PacketConnection.prototype._parseUrl = function (path) {
-  // Parse url to create the packet
-  return {};
-};
 
 module.exports = PacketConnection;
--- src/modules/UnitConnection/index.js
+++ src/modules/UnitConnection/index.js
@@ -1,93 +1,95 @@
-var _ = require('lodash'),
-    defaultUnit = require('../defaultUnit'),
+var defaultUnit = require('../defaultUnit'),
     coll = 'units';
-/**
- * Handles the connection with the units collection.
- * @constructor
- * @param {Number} id - ID of the unit.
- */
-function UnitConnection (id) {
-  this._id = id;
-  
-  // Used to keep track of db transactions that we need to wait for callbacks for. 
-  this.pendingTransactions = 0;
-}
 
-/**
- * Initiates the transactions to the db.
- * @function
- * @void
- * @param {https://github.com/mongodb/node-mongodb-native/blob/2.1/lib/db.js} db - The mongo db object.
- * @param {function} callback - Callback used to disconnect the connection to the db if all trancasctions have been completed.
- */
-UnitConnection.prototype.initTransactions = function (db, callback) {
-  // This is a function to ensure that we get the latest collection each time we access it.
-  this._collection = function() { return db.collection(coll); };
-  // Callback used to disconnect the db.
-  this._callback = callback;
+class UnitConnection { 
+  /**
+   * Handles the connection with the units collection.
+   * @constructor
+   * @param {Number} id - ID of the unit.
+   */
+  constructor(id) {
+    this._id = id;
   
-  if (!this._exists()) {
-    this._create();
+    // Used to keep track of db transactions that we need to wait for callbacks for. 
+    this.pendingTransactions = 0;
   }
-};
-
-/**
- * Gets the current settings for this unit and turns it into JSON.
- * @function
- * @return {String(JSON)} The settings for this unit as a JSON string.
- */
-UnitConnection.prototype.currentSettings = function () {
-  return JSON.stringify(this._getSettings());
-};
-
-/* 
- * "Private" methods 
- */
-
-/**
- * Gets the current settings for this unit from the db.
- * @function
- * @return {object} The settings for this unit.
- */
-// Don't need to use a callback for this transaction.
-UnitConnection.prototype._getSettings = function () {
-  return this._collection().find({ unitId: this._id }).settings;
-};
-
-/**
- * Gets the current settings for this unit.
- * @function
- * @return {object} The settings for this unit.
- */
-// Don't need to use a callback for this query.
-UnitConnection.prototype._exists = function() {
-  return (this._collection().find({ unitId: this._id }).count() !== 0);
-};
-
-/**
- * Inserts a new unit into the db.
- * @function
- * @void
- */
-UnitConnection.prototype._create = function() {
-  this.pendingQueries = this.pendingTransactions + 1;
-  this._collection.insertOne({
-    "unitId": this._id,
-    "name": "Unit " + this._collection().find().count(),
-    "settings": defaultUnit.settings,
-    "createdAt": new Date()
+  
+  /**
+   * Initiates the transactions to the db.
+   * @function
+   * @void
+   * @param {https://github.com/mongodb/node-mongodb-native/blob/2.1/lib/db.js} db - The mongo db object.
+   * @param {function} callback - Callback used to disconnect the connection to the db if all trancasctions have been completed.
+   */
+  initTransactions(db, callback) {
+    // This is a function to ensure that we get the latest collection each time we access it.
+    this._collection = function() { return db.collection(coll); };
+    // Callback used to disconnect the db.
+    this._callback = callback;
     
-    },
-    // Use _.bind to bind 'this' to the function until we get ES6 in here and use an arrow function. 
-    _.bind(function (err) {
-      if (err) throw err;
+    if (!this._exists()) {
+      this._create();
+    }
+  }
+  
+  /**
+   * Gets the current settings for this unit and turns it into JSON.
+   * @function
+   * @return {String(JSON)} The settings for this unit as a JSON string.
+   */
+  currentSettings() {
+    return JSON.stringify(this._getSettings());
+  }
+  
+  /* 
+   * "Private" methods 
+   */
+
+  /**
+   * Gets the current settings for this unit from the db.
+   * @function
+   * @return {object} The settings for this unit.
+   */
+  // Don't need to use a callback for this transaction.
+  _getSettings () {
+    return this._collection().find({ unitId: this._id }).settings;
+  }
+
+  /**
+   * Gets the current settings for this unit.
+   * @function
+   * @return {object} The settings for this unit.
+   */
+  // Don't need to use a callback for this query.
+  _exists() {
+    return (this._collection().find({ unitId: this._id }).count() !== 0);
+  }
+
+  /**
+   * Inserts a new unit into the db.
+   * @function
+   * @void
+   */
+  _create() {
+    this.pendingQueries = this.pendingTransactions + 1;
+    this._collection.insertOne({
+      "unitId": this._id,
+      "name": "Unit " + this._collection().find().count(),
+      "settings": defaultUnit.settings,
+      "createdAt": new Date()
       
-      // Remove the pending transaction from the count.
-      this.pendingTransaction = this.pendingTransaction - 1;
-      
-      // Initiate callback (_disconnect in server.js)
-      this._callback(this);
-    }, this));
-};
+      },
+      // Use arrow function to bind 'this' to the function. 
+      (err) => {
+        if (err) throw err;
+        
+        // Remove the pending transaction from the count.
+        this.pendingTransaction = this.pendingTransaction - 1;
+        
+        // Initiate callback (_disconnect in server.js)
+        this._callback(this);
+      });
+  }
+}
 
 module.exports = UnitConnection;
